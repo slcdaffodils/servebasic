9c76016691ed0f131b88370a2084d74c1e8fe229
yogeshdakre <yogeshdakre@gmail.com>
2017-11-04 22:59:45 +0530

    commiting the dbcp in a system.properties file

--- src/com/servebasic/util/DBUtil.java
+++ src/com/servebasic/util/DBUtil.java
@@ -3,12 +3,15 @@ package com.servebasic.util;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
@@ -27,9 +30,15 @@ public class DBUtil {
 	private final static Logger LOGGER = Logger.getLogger(DBUtil.class);
   static {
 	     System.out.println("Configuring the database connections.....");
-	     LOGGER.info("Configuring the database connections tomcat start up.....->"+System.getProperty("DATABASE_URL"));
+	     ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+	     InputStream input = classLoader.getResourceAsStream("system.properties");
+	     try {
+	     Properties properties = new Properties();
+	     properties.load(input);
+	     
+	     LOGGER.info("Configuring the database connections tomcat start up.....->"+properties.getProperty("DATABASE_URL"));
 	     LOGGER.info("Configuring the database connections system environment.....->"+System.getenv("DATABASE_URL"));
-	try {
+	
 		
 //		 Map<String, String> env = System.getenv();
 //	        for (String envName : env.keySet()) {
@@ -37,7 +46,7 @@ public class DBUtil {
 //	                              envName,
 //	                              env.get(envName));
 //	        }
-		    String passedURL=System.getenv("DATABASE_URL")==null?System.getProperty("DATABASE_URL"):System.getenv("DATABASE_URL");
+		    String passedURL=System.getenv("DATABASE_URL")==null?properties.getProperty("DATABASE_URL"):System.getenv("DATABASE_URL");
 			 URI dbUri = new URI(passedURL);
 			 LOGGER.info("got the URL");
 			  String dbUrl = "jdbc:mysql://" + dbUri.getHost()+":"+dbUri.getPort() + dbUri.getPath();
@@ -54,7 +63,7 @@ public class DBUtil {
 			  connectionPool.setDriverClassName("com.mysql.jdbc.Driver");
 			  connectionPool.setUrl(dbUrl);
 			  connectionPool.setInitialSize(5);
-	} catch (URISyntaxException e) {
+	} catch (Exception e) {
 		LOGGER.error("Please set up the system db url");
 		LOGGER.error(e.getMessage(),e);
 	}
